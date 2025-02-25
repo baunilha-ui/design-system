5a3dd826eedc2bfcd81e5af65338ef35f8d6f9df
Lucas Amaral <lucas.costamaral@gmail.com>
2025-02-24 21:20:43 -0300

    feat: Added cli on tokens package

--- packages/tokens/src/index.ts
+++ packages/tokens/src/index.ts
@@ -7,7 +7,9 @@ export { getLightTheme } from './theme/get-light-theme'
 
 export { getDarkTheme } from './theme/get-dark-theme'
 
-export { theme } from './theme/theme'
-
 export { createCSSVariables } from './utils/create-css-variables'
 export { applyCSSVariables } from './utils/apply-css-variables'
+
+export type { BaunilhaConfig } from './types/baunilha-config'
+
+// export { theme } from './theme/theme'
